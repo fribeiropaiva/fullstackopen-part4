2a8a0c22747c1f59ee11efeaf5848833cf824c09
Filipe Ribeiro <filipepaiva240@gmail.com>
2021-12-08 08:15:38 -0300

    Format id property on frontend and add test for it

--- controllers/blog.js
+++ controllers/blog.js
@@ -3,6 +3,7 @@ const Blog = require('../models/blog');
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({});
+
   return response.json(blogs);
 });
 
--- tests/getBlogs.test.js
+++ tests/getBlogs.test.js
@@ -4,8 +4,18 @@ const Blog = require('../models/blog');
 
 const api = supertest(app);
 
-test('returns all saved blogs', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200);
+describe('get blogs', () => {
+  test('returns all saved blogs', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200);
+  });
+
+  test('_id property is not found in any blog', async () => {
+    const blogs = await api.get('/api/blogs');
+
+    const checkIds = blogs.body.some((blog) => blog._id);
+
+    expect(checkIds).toBe(false);
+  });
 });
